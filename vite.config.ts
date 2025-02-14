1eb93abc4fbb792fc92442068b2e4cd5e3852aea
Plarget <67307148+Plarget@users.noreply.github.com>
2025-02-14 18:29:18 +0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      { find: "@", replacement: path.resolve(__dirname, "src") }
-    ]
-  }
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    extensions: [".js", ".ts", ".jsx", ".tsx"],
+  },
 });
